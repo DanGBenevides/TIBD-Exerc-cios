61ab997dfd54419a9a6c1d33d55f274d3e5dcad1
Daniel <90224207+DanGBenevides@users.noreply.github.com>
2022-04-28 18:54:17 -0300

    Update Translate.java

--- Exercicio4/Translate.java
+++ Exercicio4/Translate.java
@@ -1,1 +1,102 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import com.google.gson.*;
+import com.squareup.okhttp.*;
 
+public class Translate {
+    private static String key = "d06aa7e82da44ffca34b6c5a3e050ead";
+    private static String location = "eastus";
+
+    HttpUrl url = new HttpUrl.Builder()
+        .scheme("https")
+        .host("api.cognitive.microsofttranslator.com")
+        .addPathSegment("/translate")
+        .addQueryParameter("api-version", "3.0")
+        .addQueryParameter("from", "en")
+        .addQueryParameter("to", "de")
+        .addQueryParameter("to", "it")
+        .build();
+
+    // Instantiates the OkHttpClient.
+    OkHttpClient client = new OkHttpClient();
+
+    // This function performs a POST request.
+    public String Post() throws IOException {
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType,
+                "[{\"Text\": \"Hello World!\"}]");
+        Request request = new Request.Builder().url(url).post(body)
+                .addHeader("Ocp-Apim-Subscription-Key", key)
+                .addHeader("Ocp-Apim-Subscription-Region", location)
+                .addHeader("Content-type", "application/json")
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    // This function prettifies the json response.
+    public static String prettify(String json_text) {
+        JsonParser parser = new JsonParser();
+        JsonElement json = parser.parse(json_text);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(json);
+    }
+
+    public static void main(String[] args) {
+        try {
+            Translate translateRequest = new Translate();
+            String response = translateRequest.Post();
+            System.out.println(prettify(response));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+public class Detect {
+    private static String key = "d06aa7e82da44ffca34b6c5a3e050ead";
+    private static String location = "eastus";
+
+    HttpUrl url = new HttpUrl.Builder()
+        .scheme("https")
+        .host("api.cognitive.microsofttranslator.com")
+        .addPathSegment("/detect")
+        .addQueryParameter("api-version", "3.0")
+        .build();
+
+    // Instantiates the OkHttpClient.
+    OkHttpClient client = new OkHttpClient();
+
+    // This function performs a POST request.
+    public String Post() throws IOException {
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType,
+                "[{\"Text\": \"Ich würde wirklich gern Ihr Auto um den Block fahren ein paar Mal.\"}]");
+        Request request = new Request.Builder().url(url).post(body)
+                .addHeader("Ocp-Apim-Subscription-Key", key)
+                .addHeader("Ocp-Apim-Subscription-Region", location)
+                .addHeader("Content-type", "application/json")
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    // This function prettifies the json response.
+    public static String prettify(String json_text) {
+        JsonParser parser = new JsonParser();
+        JsonElement json = parser.parse(json_text);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(json);
+    }
+
+    public static void main(String[] args) {
+        try {
+            Detect detectRequest = new Detect();
+            String response = detectRequest.Post();
+            System.out.println(prettify(response));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
